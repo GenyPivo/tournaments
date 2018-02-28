726a7d5f45245b914a1ceae4124454098cfa0861
Evgeniy Pivovarov <geny.pivo@gmail.com>
2018-02-28 13:44:58 +0200

    Log in/Log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to log_in_path, :notice => 'Page only for authorized users'
+    end
+  end
 end
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,11 @@
 class RootController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
+
+  end
+
+  def log_in
 
   end
 end
